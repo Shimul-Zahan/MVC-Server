999a3cf9b21c71b4cb602ecbeebb06e207e7f3ff
AlgoWebBot <shimulzahan636@gmail.com>
2024-11-18 00:10:41 +0600

    update conversation backend

--- src/routes/conversation.route.js
+++ src/routes/conversation.route.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const trimRequest = require('trim-request')
 const { authMiddlewareFunction } = require('../middlewares/auth.middleware')
-const create_open_conversation = require('../controllers/conversation.controller')
+const { create_open_conversation, getAllConversationForAUser } = require('../controllers/conversation.controller')
 
 router.route('/').post(trimRequest.all, authMiddlewareFunction, create_open_conversation)
+
+router.route('/').get(trimRequest.all, authMiddlewareFunction, getAllConversationForAUser)
 
 
 module.exports = router
